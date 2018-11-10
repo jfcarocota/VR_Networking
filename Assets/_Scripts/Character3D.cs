b3e740e146b3bd3957181739aa6764004bb0ff12
jfcarocota <jfcarocota@gmail.com>
2018-11-10 10:34:31 -0700

    Movement and rotaion

---
+++ Assets/_Scripts/Character3D.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.SystemControls;
+
+public abstract class Character3D : MonoBehaviour
+{
+    [SerializeField]
+    protected float moveSpeed;
+
+    [SerializeField]
+    protected float rotationSpeed;
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    protected virtual void Movement()
+    {
+        SystemControls.Movement(transform, moveSpeed, rotationSpeed);
+    }
+}
+
